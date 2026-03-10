a9fdc245bd4c63c3b0ce24389f5cbfd3e1f662d8
yelnnee <y.adouane@ecole-ipssi.net>
2026-03-10 12:40:09 +0100

    exo 02

---
+++ dataset.py
@@ -0,0 +1,16 @@
+# Données
+x = [50, 70, 90]
+y = [100, 140, 180]
+
+# Moyennes
+xm = sum(x) / len(x)
+ym = sum(y) / len(y)
+
+# Coefficient a
+a = sum((xi - xm) * (yi - ym) for xi, yi in zip(x, y)) / \
+    sum((xi - xm) ** 2 for xi in x)
+
+# Intercept b
+b = ym - a * xm
+
+print(a, b)
