d0cad92f4b7c02f7e62d28c18d2b1b77548118fd
yelnnee <y.adouane@ecole-ipssi.net>
2026-03-11 10:03:52 +0100

    modif dataset

--- dataset-02.py
+++ dataset-02.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-# Nouveau dataset 
-X = np.array([30, 45, 60, 75, 90])
-Y = np.array([90, 130, 170, 210, 250])
+# Dataset 1 
+X = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+Y = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68])
 
-# matrice X avec biais
+
 X_matrix = np.column_stack((np.ones(len(X)), X))
 
-# calcul analytique 
+# calcul analytique
 theta = np.linalg.inv(X_matrix.T @ X_matrix) @ X_matrix.T @ Y
 
-b = theta[0]   
-a = theta[1]   
+b = theta[0]
+a = theta[1]
 
 print("pente a =", a)
 print("biais b =", b)
